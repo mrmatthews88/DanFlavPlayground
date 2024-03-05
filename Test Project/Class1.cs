83a253bbc26e794aa25d777e3c33b191b8d54779
Mr D. Matthews <mr.drmatthews@gmail.com>
2024-03-05 21:05:20 +0000

    argh

---
+++ Test Project/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_Project.Database;
+
+namespace Test_Project
+{
+    internal class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            var ctx = new DatabaseContext();
+
+            var data = ctx.YourEntities.ToList();
+
+
+            var a = 1;
+            Assert.AreEqual(1, a);
+        }
+    }
+}
---
+++ Test Project/Database/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test_Project.Database
+{
+    internal class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlite("Data Source=your_database.db");
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=FlatDanTestDb;Trusted_Connection=True;");
+        }
+
+
+        public DbSet<YourEntity> YourEntities { get; set; }
+
+    }
+
+    public class YourEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Add other properties as needed
+    }
+}
